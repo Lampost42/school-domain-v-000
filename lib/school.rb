6f6966ab309e49e4133aa2c0a85f3bc1426481ad
Trevor Russell <truss.42@gmail.com>
2018-06-18 19:36:31 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,8 +1,8 @@
 class School
   def initialize(school_name)
     @school_name = school_name
-    roster = {}
   end
+      roster = {}
   def add_student(name, grade)
     roster[grade] = []
     roster[grade] << name
